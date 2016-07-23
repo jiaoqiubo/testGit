08a0272323591fa41fbd9c4fbe5d32466f7e43bb
jiaoqiubo <1140771579@qq.com>
2016-07-23 06:45:07 -0700

    测试文件

---
+++ ceshi/ceshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshi
+//
+//  Created by jiaoqiubo on 16/7/23.
+//  Copyright © 2016年 jiaoqiubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ceshi/ceshi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ceshi
+//
+//  Created by jiaoqiubo on 16/7/23.
+//  Copyright © 2016年 jiaoqiubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
